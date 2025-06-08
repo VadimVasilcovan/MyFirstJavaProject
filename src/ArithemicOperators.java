2d8709ec68994f6320af141552d5554fcf43ca62
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-06-08 23:35:33 +0300

    Arithmetic Operation in Java

---
+++ src/ArithemicOperators.java
@@ -0,0 +1,46 @@
+public class ArithemicOperators {
+    public static void main(String[] args){
+        //Arithmetic Operators
+        int x = 10;
+        int y = 3;
+        int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+
+        //using % to determinate if the number is iven or not
+        //z = x % y;
+
+        //Augmented Assignment Operators
+        //x = x+ y or
+        //x += y;
+
+        //x = x- y or
+        //x -= y;
+
+
+        //x = x * y or
+        //x *= y;
+
+
+        //x = x/y or
+        //x /= y;
+
+
+        //x = x % y or
+        //x %= y;
+
+        //increment operator
+        // x = x+ 1 or
+        // x++
+        // x = x- 1 or
+        //x--
+
+        //ORDERS OF OPERATIONS [P-E-M-D-A-S]
+        double result = 3 + 4 * (7-5) / 2.0;
+        System.out.println(result);
+        System.out.print(x);
+    }
+}
