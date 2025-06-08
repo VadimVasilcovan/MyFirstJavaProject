c6067a62023177da1a38bf94e71344e83b64095f
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-06-09 00:49:58 +0300

    ifStatement

--- src/ifStatemenet.java
+++ src/ifStatemenet.java
@@ -1,2 +1,13 @@
 public class ifStatemenet {
+    public static void main(String[] args){
+    int age = 13;
+    if(age >=18){
+        System.out.println("you are an adult");
+    }else if(age < 0){
+        System.out.println("you haven't been born yet!");
+        }else{
+        System.out.println("suck dick");
+
+        }
+    }
 }
