20a7925559ff2f4bc690654d191e3f952c98df1f
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-06-09 00:28:58 +0300

    09.06.2025

--- src/shoppingCartProgram.java
+++ src/shoppingCartProgram.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class shoppingCartProgram {
+    public static void main(String[] args){
+    String itemName = "";
+    double price = 0;
+    int quantity =0;
+    double totalPrice = 0;
+    Scanner scanner = new Scanner((System.in));
+        System.out.print("What item would you like to buy:?");
+        itemName = scanner.nextLine();
+        System.out.print("What is the price for each ?:");
+        price = scanner.nextDouble();
+        System.out.print("How many would you like?: ");
+        quantity = scanner.nextInt();
+
+        totalPrice = price * quantity;
+        System.out.println("Item name" + itemName);
+        System.out.println("price for each " + price );
+        System.out.println("total" + totalPrice );
+     scanner.close();
+
+    }
 }
