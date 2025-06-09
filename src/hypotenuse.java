02b587b440fb753474488432907b10717e37ac18
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-06-09 15:16:38 +0300

    Math exercises

--- src/hypotenuse.java
+++ src/hypotenuse.java
@@ -6,7 +6,7 @@ public class hypotenuse {
 
         Scanner scanner = new Scanner(System.in);
 
-        double a;
+     /*   double a;
         double b;
         double c;
 
@@ -17,6 +17,45 @@ public class hypotenuse {
         b = scanner.nextDouble();
         c =Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
         System.out.println("the result is " + c + "cm");
+
+        //circumference = 2 * Math.PI * radius.
+    */
+       /* double radius;
+        double circumference;
+
+        System.out.print("Add the radius: ");
+        radius = scanner.nextDouble();
+        circumference = 2 * Math.PI * radius;
+
+
+        System.out.println("the circumference is: " + circumference);*/
+
+      /*  double area;
+        double radius;
+
+        System.out.println("write the area: ");
+        radius = scanner.nextDouble();
+
+        area = Math.PI * Math.pow(radius, 2);
+        System.out.println( "the area is:" + area);*/
+
+
+
+        double radius;
+        double volume;
+
+        System.out.print("add radius: ");
+        radius = scanner.nextDouble();
+
+
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.println("the result is: " + volume + "m^2");
+
+
+
         scanner.close();
+
+
     }
 }
