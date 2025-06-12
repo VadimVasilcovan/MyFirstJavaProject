b36785446306db5fb7ae072fa963779067428fe9
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-06-12 14:40:30 +0300

    user input on array

---
+++ src/enterUserInputIntoAnArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class enterUserInputIntoAnArray {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String[] foods;
+        int size;
+      
+
+        System.out.print("What number of food do you want?: ");
+        size = scanner.nextInt();
+        scanner.nextLine();
+        foods = new String[size];
+        for(int i = 0; i <foods.length; i++){
+            System.out.println("Enter a food: ");
+            foods[i] = scanner.nextLine();
+        }
+        System.out.println(foods.length);
+        for(String food : foods){
+            System.out.println(food);
+        }
+        scanner.close();
+    }
+}
