b6e2126bf1221aeec10919d9fd4c6bb2da3f8d63
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-06-13 23:35:30 +0300

    Update slotMachine

--- src/slotMachine.java
+++ src/slotMachine.java
@@ -1,15 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class slotMachine {
+
     public static void main(String[] args){
-        String[][] slotFruits = {{"🍒","🍒","🍒","🍒","🍒"},
-                                 {"🍋","🍋","🍋","🍋","🍋"},
-                                 {"⭐","⭐","⭐","⭐","⭐"},
-                                 {"🍎","🍎","🍎","🍎","🍎"},
-                                 {"🍇","🍇","🍇","🍇","🍇"}};
+        String[][] slotFruits = {{"🍒","🍋","🍇","⭐","🍎"},
+                                 {"🍒","🍋","🍇","⭐","🍎"},
+                                 {"🍒","🍋","🍇","⭐","🍎"},
+                                 {"🍒","🍋","🍇","⭐","🍎"},
+                                 {"🍒","🍋","🍇","⭐","🍎"}};
         int randomElement;
+        boolean allMatch = true;
         String[] result = new String[slotFruits.length];
         Random random = new Random();
+
 
         for(int i = 0; i < slotFruits.length; i++)
             for(int j = 0; j < 1; j++){
@@ -19,5 +23,42 @@ public class slotMachine {
       for(String res : result){
           System.out.print(res);
       }
+
+
+
+         do{
+             for (int i = 0; i< result.length; i++){
+                 if (!result[i].equals(result[0])) {
+                     allMatch = false;
+                     System.out.println("the elements are not the same you loose your money");
+                     break;
+                 }
+         }
+
+     }while(allMatch);
+    }
+
+
+       static void play (int result){
+        double yourAmount;
+        double yourBet = 0;
+        double yourGain = yourBet * 5;
+        int spinRepetition ;
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter your bet: ");
+            yourBet =scanner.nextDouble();
+            System.out.println("You want to spin 1 0r 10 times");
+            spinRepetition = scanner.nextInt();
+            if(spinRepetition != 1 && spinRepetition !=10){
+                spinRepetition = 0;
+                System.out.println("this is not a valid number please enter 1 or 10");
+
+            }
+            scanner.close();
+
+
+
+
     }
 }
