df97a11c0a8d0d68194e44825f31b10c432038de
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-06-14 00:18:14 +0300

    spin Game

--- src/slotMachine.java
+++ src/slotMachine.java
@@ -5,67 +5,83 @@ public class slotMachine {
 
     public static void main(String[] args){
         String[][] slotFruits = {{"🍒","🍋","🍇","⭐","🍎"},
-                                 {"🍒","🍋","🍇","⭐","🍎"},
-                                 {"🍒","🍋","🍇","⭐","🍎"},
-                                 {"🍒","🍋","🍇","⭐","🍎"},
-                                 {"🍒","🍋","🍇","⭐","🍎"}};
+                {"🍒","🍋","🍇","⭐","🍎"},
+                {"🍒","🍋","🍇","⭐","🍎"},
+                {"🍒","🍋","🍇","⭐","🍎"},
+                {"🍒","🍋","🍇","⭐","🍎"}};
         int randomElement;
-        boolean allMatch = true;
+        boolean allMatch;
         String[] result = new String[slotFruits.length];
         Random random = new Random();
-        int spins = play(allMatch);
 
-        for(int spinCount = 0; spinCount <spins; spinCount++){
-            for(int i = 0; i < slotFruits.length; i++)
-                for(int j = 0; j < 1; j++){
-                    randomElement = random.nextInt(slotFruits[i].length);
-                    result[i] = slotFruits[i][randomElement];
-                }
+       
+        GameInput input = play();
+
+        double yourBet = input.yourBet;
+        int spins = input.spinRepetition;
+        double yourAmount = 0;
+        double yourGain = yourBet * 5;
+
+        for(int spinCount = 0; spinCount < spins; spinCount++){
+            allMatch = true;
+
+            for(int i = 0; i < slotFruits.length; i++){
+                randomElement = random.nextInt(slotFruits[i].length);
+                result[i] = slotFruits[i][randomElement];
+            }
+
             for(String res : result){
                 System.out.print(res);
             }
+            System.out.println();
 
+            for (int i = 0; i < result.length; i++){
+                if (!result[i].equals(result[0])) {
+                    allMatch = false;
 
-
-            do{
-                for (int i = 0; i< result.length; i++){
-                    if (!result[i].equals(result[0])) {
-                        allMatch = false;
-                        System.out.println("the elements are not the same you loose your money");
-                        break;
-                    }
+                    System.out.println("The elements are not the same, you lose your money.");
+                    break;
                 }
+            }
 
-            }while(allMatch);
+            if(allMatch){
+                yourAmount += yourGain;
+                System.out.println("All elements match! You win " + yourGain);
+            }
         }
 
+        System.out.println("Total amount won: " + yourAmount);
+        System.out.println("Want to try one more time yes/no?");
 
     }
 
+    static class GameInput {
+        double yourBet;
+        int spinRepetition;
 
-       static int play (boolean allMatch){
-        double yourAmount;
-        double yourBet = 0;
-        double yourGain = yourBet * 5;
-        int spinRepetition ;
+        GameInput(double bet, int spins) {
+            yourBet = bet;
+            spinRepetition = spins;
+        }
+    }
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter your bet: ");
-            yourBet =scanner.nextDouble();
+    static GameInput play(){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your bet: ");
+        double yourBet = scanner.nextDouble();
 
-            do{
-                System.out.println("You want to spin 1 0r 10 times");
-                spinRepetition = scanner.nextInt();
-                if(spinRepetition != 1 && spinRepetition !=10){
-                    spinRepetition = 0;
-                    System.out.println("this is not a valid number please enter 1 or 10");
-                }
-            }while (spinRepetition != 1 && spinRepetition != 10);
-            scanner.close();
-           return spinRepetition;
-
+        int spinRepetition;
+        do{
+            System.out.println("You want to spin 1 or 10 times");
+            spinRepetition = scanner.nextInt();
+            if(spinRepetition != 1 && spinRepetition != 10){
+                System.out.println("This is not a valid number. Please enter 1 or 10.");
+            }
+        } while (spinRepetition != 1 && spinRepetition != 10);
 
 
+
+        return new GameInput(yourBet, spinRepetition);
     }
 }
